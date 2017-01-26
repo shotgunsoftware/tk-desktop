1f56133e077b312244953be21d7b5a938461a554
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-01-25 22:09:23 -0500

    removing files we haven't really modified

--- python/tk_desktop/ui/update_project_config.py
+++ python/tk_desktop/ui/update_project_config.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'update_project_config.ui'
 #
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
 
