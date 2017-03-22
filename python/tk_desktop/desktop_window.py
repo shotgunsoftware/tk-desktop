shotgunsoftware/tk-desktop

commit 476a49c7bfa216eb441b1ea44a1b27be0e5df995

For #36439, ensure desktop uses the right credentials This ensures that both site and project level engines use the same credentials. The site engine now passes down the credentials to the project engine in the data file. Closes https://github.com/shotgunsoftware/tk-desktop/pull/59

python/tk_desktop/desktop_window.py
@@ -817,6 +817,10 @@ class DesktopWindow(SystrayWindow):
         # startup server pipe to listen
         engine.startup_rpc()
 
+        # Make sure the credentials are refreshed so the background process
+        # has no problem launching.
+        engine.refresh_user_credentials()
+
         # pickle up the info needed to bootstrap the project python
         desktop_data = {
             "core_python_path": core_python,
@@ -839,20 +843,22 @@ class DesktopWindow(SystrayWindow):
 
         # Check if the pipeline configuration is login based.
         engine.check_login_based(core_root)
-        # Make sure the credentials are refreshed so the background process
-        # has no problem launching.
-        engine.refresh_user_credentials()
-
         # Ticket 26741: Avoid having odd DLL loading issues on windows
         self._push_dll_state()
 
         engine.log_info("--- launching python subprocess (%s)" % path_to_python)
-        engine.execute_hook(
-            "hook_launch_python",
-            project_python=path_to_python,
-            pickle_data_path=pickle_data_file,
-            utilities_module_path=utilities_module_path,
+        os.environ["SHOTGUN_DESKTOP_CURRENT_USER"] = sgtk.authentication.serialize_user(
+            sgtk.get_authenticated_user()
         )
+        try:
+            engine.execute_hook(
+                "hook_launch_python",
+                project_python=path_to_python,
+                pickle_data_path=pickle_data_file,
+                utilities_module_path=utilities_module_path,
+            )
+        finally:
+            del os.environ["SHOTGUN_DESKTOP_CURRENT_USER"]
 
         self._pop_dll_state()
 

python/utils/bootstrap_utilities.py
@@ -31,9 +31,23 @@ def start_engine(data):
     # a current user, we have to set the authenticated user.
     if hasattr(sgtk, "set_authenticated_user"):
         # Retrieve the currently authenticated user for this process.
-        from tank_vendor.shotgun_authentication import ShotgunAuthenticator
-        user = ShotgunAuthenticator(sgtk.util.CoreDefaultsManager()).get_default_user()
-        sgtk.set_authenticated_user(user)
+        from tank_vendor.shotgun_authentication import ShotgunAuthenticator, deserialize_user
+        current_user = ShotgunAuthenticator(sgtk.util.CoreDefaultsManager()).get_default_user()
+
+        # If we found no user using the authenticator, we need to use the credentials that
+        # came through the environment variable.
+        # Also, if the credentials are user-based, we need to disregard what we got and use
+        # the credentials from the environment variable. This is required to solve any issues
+        # arising from the changes to the session cache changing place in core 0.18.
+        if not current_user or current_user.login:
+            current_user = deserialize_user(os.environ["SHOTGUN_DESKTOP_CURRENT_USER"])
+        else:
+            # This happens when the user retrieved from the project's core is a script.
+            # In that case, we use the script user and disregard who is the current
+            # authenticated user at the site level.
+            pass
+
+        sgtk.set_authenticated_user(current_user)
 
     tk = sgtk.sgtk_from_path(data["config_path"])
     tk._desktop_data = data["proxy_data"]
@@ -84,6 +98,7 @@ def start_app(engine):
         # wait for the engine communication channel to shut down
         engine.msg_server.join()
         return 0
+
 
 def handle_error(data):
     """

commit 39b9674cc337b59c2d5d61149a9d08882128116b

For #36439, ensure desktop uses the right credentials This fixes a regression that was introduced by https://github.com/shotgunsoftware/tk-desktop/commit/476a49c7bfa216eb441b1ea44a1b27be0e5df995. The fix is now compatible with Shotgun Desktop versions prior to 1.1.

python/tk_desktop/desktop_window.py
@@ -847,8 +847,12 @@ class DesktopWindow(SystrayWindow):
         self._push_dll_state()
 
         engine.log_info("--- launching python subprocess (%s)" % path_to_python)
+        # Get the human user we authenticated as. Do not use get_authenticated_user. If the
+        # desktop is running on top of Shotgun Desktop 1.02, then desktop runs in legacy
+        # more where the authenticated user is None. Instead, use engine.get_current_user(),
+        # which knows the user used to actually log into desktop.
         os.environ["SHOTGUN_DESKTOP_CURRENT_USER"] = sgtk.authentication.serialize_user(
-            sgtk.get_authenticated_user()
+            engine.get_current_user()
         )
         try:
             engine.execute_hook(