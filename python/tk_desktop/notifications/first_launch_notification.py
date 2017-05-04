61aec95d2982f1fc0cc533c147b2170e3c670dec
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-05-04 14:10:02 -0400

    first launch notification now takes you to user guide.

--- python/tk_desktop/notifications/first_launch_notification.py
+++ python/tk_desktop/notifications/first_launch_notification.py
@@ -21,8 +21,7 @@ class FirstLaunchNotification(Notification):
 
     _FIRST_LAUNCH_BANNER_VIEWED_ID = "first_desktop_launch_banner_viewed"
     _SHOTGUN_DESKTOP_SUPPORT_PAGE_URL = (
-        r"https://support.shotgunsoftware.com/hc/en-us/articles/219040668-"
-        "Shotgun-Desktop-Download-and-Setup#The%20Toolkit%20Project%20setup%20wizard"
+        "https://support.shotgunsoftware.com/hc/en-us/articles/115000068574"
     )
 
     @classmethod
