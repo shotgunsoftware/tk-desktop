shotgunsoftware/tk-desktop

commit 5fb929fd788ea0dc9082088c748aa139f3aa0ec6

SPLAT-12: Adds support for site level desktop tabs  (#75)
- Adds the ability to dock panel apps in the desktop site level UI.
- Adds a new run_at_startup setting to the desktop site configuration which allows for apps to run at startup.

These two features in conjunction makes it easy to configure multiple tabs in desktop.

engine.py
@@ -65,6 +65,27 @@ class DesktopEngine(Engine):
         if hasattr(self.__impl, "post_app_init"):
             self.__impl.post_app_init()
 
+    def show_panel(self, panel_id, title, bundle, widget_class,
+                   *args, **kwargs):
+        """
+        Shows the panel in the desktop engine, if supported by the current
+        desktop mode (site vs project).
+
+        :param panel_id:     Unique identifier for the panel, as obtained by
+                             register_panel().
+        :param title:        The title of the panel.
+        :param bundle:       The app, engine or framework object that is
+                             associated with this window.
+        :param widget_class: The class of the UI to be constructed. This must
+                             derive from QWidget.
+
+        Additional parameters specified will be passed through to the
+        widget_class constructor.
+        """
+        if hasattr(self.__impl, "show_panel"):
+            self.__impl.show_panel(panel_id, title, bundle, widget_class,
+                                   *args, **kwargs)
+
     def destroy_engine(self):
         """ Clean up the engine """
         self.logger.debug("destroy_engine")

info.yml
@@ -33,6 +33,30 @@ configuration:
                      commands that do not match any of the values in the groups
                      setting."
 
+    run_at_startup:
+        type: list
+        description: "Controls what apps will run on startup.  This is a list
+                     where each element is a dictionary with two keys:
+                     'app_instance' and 'name'.
+                     The 'app_instance' value connects this entry to a
+                     particular app instance defined in the environment
+                     configuration file.
+                     The 'name' is the menu name of the command to run when
+                     desktop starts up. If 'name' is '', then all commands from
+                     the given app instance are started.
+
+                     To add the 'Apps' tab (which contains the app launcher),
+                     add the following startup command to the list:
+                       {app_instance: '', name: 'Apps'}
+                     "
+        allows_empty: True
+        default_value: [{app_instance: '', name: Apps}]
+        values:
+            type: dict
+            items:
+                name: { type: str }
+                app_instance: { type: str }
+
     groups:
         type: list
         description: "A list of dictionaries that define what commands get put

python/tk_desktop/desktop_engine_site_implementation.py
@@ -16,6 +16,7 @@ import string
 import collections
 
 from sgtk.errors import TankEngineInitError
+from sgtk.platform import Engine
 
 from distutils.version import LooseVersion
 import sgtk
@@ -51,6 +52,99 @@ class DesktopEngineSiteImplementation(object):
     def destroy_engine(self):
         shotgun_globals.unregister_bg_task_manager(self._task_manager)
         self.site_comm.shut_down()
+
+
+    ###########################################################################
+    # panel support (displayed as tabs)
+    def _run_startup_commands(self):
+        """
+        Runs the commands that are configured to be executed at startup, through
+        the 'run_at_startup' setting in the configuration.
+
+        This gives an opportunity for apps to display panels through the
+        execution of a command, which will add a tab to Desktop.
+        """
+        apps_selector = {"app_instance": "", "name": "Apps"}
+
+        selectors = self._engine.get_setting("run_at_startup", [apps_selector])
+
+        # "Apps" is currently a builtin command, so we take care of it
+        # separately.
+        # We first figure out what is its index in the selectors, or None if it
+        # is not present
+        apps_index = (selectors.index(apps_selector)
+                      if apps_selector in selectors else None)
+        # strip the "Apps" tab from the selectors, as we handle it separately
+        if apps_index is not None:
+            del selectors[apps_index]
+
+        commands = self._engine.get_matching_commands(selectors)
+
+        # add the "Apps" tab back at the appropriate position
+        if apps_index is not None:
+            # we can't blindly use the previous index, since the following
+            # special cases can occur:
+            # - commands were not found (a selector with no matching command)
+            # - multiple commands were found (a wildcard selector found many)
+            #
+            # Thus, we go through the matched commands until we have run through
+            # all the commands that should occur before our "Apps" tab (based on
+            # its location in the selectors).
+            # In other words, we find where the "Apps" tab belongs in the
+            # matched commands.
+            apps_command_index = 0
+            # skip the commands from the selectors preceding "Apps"
+            for index in xrange(apps_index):
+                selector = selectors[index]
+                # skip all the commands that fit the current selector
+                while apps_command_index < len(commands): # insert as the last
+                                                          # command in the worst
+                                                          # case
+                    app, name, _ = commands[apps_command_index]
+
+                    # only keep skipping commands if the current selector
+                    # matches the current command
+                    if (selector["app_instance"] != app or
+                        (selector["name"] != "" and selector["name"] != name)):
+                        break
+
+                    apps_command_index += 1
+
+            # we now have the index where the apps command should be and we can
+            # add a fake command callback that will register the "Apps" tab.
+            apps_tab_callback = self.desktop_window._register_apps_tab
+            commands.insert(apps_command_index, ("", "", apps_tab_callback))
+
+
+        # Execute the actual commands.
+        # For example, a command could be displaying a panel in order to
+        # display it as a tab in the desktop.
+        for (_,_,command_callback) in commands:
+            command_callback()
+
+    def show_panel(self, panel_id, title, bundle, widget_class,
+                   *args, **kwargs):
+        """
+        Adds an app widget as a tab in the desktop UI. The tab is placed in the
+        next available tab slot in the desktop, going from left to right.
+
+        :param panel_id:     Unique identifier for the panel, as obtained by
+                             register_panel().
+        :param title:        The title of the panel, which is the title
+                             displayed for the tab in the UI.
+        :param bundle:       The app, engine or framework object that is
+                             associated with this window.
+        :param widget_class: The class of the UI to be constructed. This must
+                             derive from QWidget.
+
+        Additional parameters specified will be passed through to the
+        widget_class constructor.
+        """
+        self._engine.log_debug("Registering panel \"%s\" (id %s) as a tab." %
+                               (title, panel_id))
+        widget = widget_class(*args, **kwargs)
+
+        self.desktop_window.register_tab(title, widget)
 
     def startup_rpc(self):
         self.site_comm.start_server()
@@ -323,6 +417,9 @@ class DesktopEngineSiteImplementation(object):
                 "with the Shotgun Integrations. Please install the latest version."
 
             )
+
+        # run the commands that are configured to be executed at startup
+        self._run_startup_commands()
 
         # make sure we close down our rpc threads
         app.aboutToQuit.connect(self._engine.destroy_engine)

python/tk_desktop/desktop_window.py
@@ -120,10 +120,7 @@ class DesktopWindow(SystrayWindow):
         QtGui.QApplication.instance().setQuitOnLastWindowClosed(False)
 
         # Setup header buttons
-        self.ui.apps_button.setProperty("active", True)
-        self.ui.apps_button.style().unpolish(self.ui.apps_button)
-        self.ui.apps_button.style().polish(self.ui.apps_button)
-
+        engine = sgtk.platform.current_engine()
         connection = engine.get_current_user().create_sg_connection()
 
         # Setup the console
@@ -350,9 +347,9 @@ class DesktopWindow(SystrayWindow):
         return ret
 
     def _push_dll_state(self):
-        '''
+        """
         Push current Dll Directory
-        '''
+        """
         if sys.platform == "win32":
             try:
                 import win32api
@@ -368,15 +365,76 @@ class DesktopWindow(SystrayWindow):
                 log.warning("Could not push DllDirectory under Windows.")
 
     def _pop_dll_state(self):
-        '''
+        """
         Pop the previously pushed DLL Directory
-        '''
+        """
         if sys.platform == "win32":
             try:
                 import win32api
                 win32api.SetDllDirectory(self._previous_dll_directory)
             except StandardError:
                 log.warning("Could not restore DllDirectory under Windows.")
+
+    def register_tab(self, tab_name, tab_widget):
+        """
+        Register a tab to add to the UI
+
+        :param tab_name:   Name displayed on the tab button.
+        :param tab_widget: Widget to display for the tab.
+        """
+        # setup the header button for the tab
+        tab_button = QtGui.QPushButton(self.ui.header)
+
+        # button behaviour/styling
+        tab_button.setMouseTracking(True)
+        tab_button.setFocusPolicy(QtCore.Qt.NoFocus)
+        tab_button.setFlat(True)
+        tab_button.setProperty("active", False)
+
+        # tab-specific values
+        tab_button.setText(tab_name)
+
+        # setup the tab widget
+        tab_widget.setParent(self)
+
+
+        # define the event handler when the user changes tab
+        def on_tab_selected():
+            """
+            Event fired when a tab is selected by the user
+            """
+            # update the state of tab buttons
+            for i in xrange(self.ui.tabs.count()):
+                button = self.ui.tabs.itemAt(i).widget()
+                button.setProperty("active", button == tab_button)
+                # apply style update
+                button.style().unpolish(button)
+                button.style().polish(button)
+
+            # display the new tab content
+            self.ui.tab_view.setCurrentWidget(tab_widget)
+
+        # link the button to the page widget
+        tab_button.toggled.connect(on_tab_selected)
+        tab_button.clicked.connect(on_tab_selected)
+
+
+        # add the tab components to the ui
+        self.ui.tabs.addWidget(tab_button)
+        self.ui.tab_view.addWidget(tab_widget)
+
+        # select tab if this is the first one
+        if self.ui.tabs.count() == 1:
+            on_tab_selected()
+
+
+    def _register_apps_tab(self):
+        """
+        Registers the "Apps" tab, which allows users to launch toolkit apps.
+        This should eventually be moved to an app on its own.
+        """
+        self.register_tab("Apps", self.ui.apps_tab)
+
 
     ########################################################################################
     # Event handlers and slots
@@ -1300,9 +1358,9 @@ class DesktopWindow(SystrayWindow):
                 del os.environ["SHOTGUN_DESKTOP_CURRENT_USER"]
 
     def slide_view(self, new_page, from_direction="right"):
-        offsetx = self.ui.stack.frameRect().width()
-        offsety = self.ui.stack.frameRect().height()
-        current_page = self.ui.stack.currentWidget()
+        offsetx = self.ui.apps_tab.frameRect().width()
+        offsety = self.ui.apps_tab.frameRect().height()
+        current_page = self.ui.apps_tab.currentWidget()
 
         new_page.setGeometry(0, 0, offsetx, offsety)
 
@@ -1331,7 +1389,7 @@ class DesktopWindow(SystrayWindow):
         anim_group.addAnimation(anim_new)
 
         def slide_finished():
-            self.ui.stack.setCurrentWidget(new_page)
+            self.ui.apps_tab.setCurrentWidget(new_page)
         anim_group.finished.connect(slide_finished)
         anim_group.start()
 

python/tk_desktop/ui/desktop_window.py
@@ -47,23 +47,16 @@ class Ui_DesktopWindow(object):
         self.horizontalLayout_2.setSpacing(20)
         self.horizontalLayout_2.setContentsMargins(20, 0, 20, 0)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.apps_button = QtGui.QPushButton(self.header)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.apps_button.sizePolicy().hasHeightForWidth())
-        self.apps_button.setSizePolicy(sizePolicy)
-        self.apps_button.setMinimumSize(QtCore.QSize(0, 0))
-        self.apps_button.setMouseTracking(True)
-        self.apps_button.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.apps_button.setFlat(True)
-        self.apps_button.setObjectName("apps_button")
-        self.horizontalLayout_2.addWidget(self.apps_button)
+        self.tabs = QtGui.QHBoxLayout()
+        self.tabs.setObjectName("tabs")
+        self.horizontalLayout_2.addLayout(self.tabs)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem)
         self.border_layout.addWidget(self.header)
-        self.stack = QtGui.QStackedWidget(self.center)
-        self.stack.setObjectName("stack")
+        self.tab_view = QtGui.QStackedWidget(self.center)
+        self.tab_view.setObjectName("tab_view")
+        self.apps_tab = QtGui.QStackedWidget()
+        self.apps_tab.setObjectName("apps_tab")
         self.project_browser_page = QtGui.QWidget()
         self.project_browser_page.setObjectName("project_browser_page")
         self.verticalLayout = QtGui.QVBoxLayout(self.project_browser_page)
@@ -164,7 +157,7 @@ class Ui_DesktopWindow(object):
         self.projects.setSelectionRectVisible(False)
         self.projects.setObjectName("projects")
         self.verticalLayout.addWidget(self.projects)
-        self.stack.addWidget(self.project_browser_page)
+        self.apps_tab.addWidget(self.project_browser_page)
         self.project_page = QtGui.QWidget()
         self.project_page.setObjectName("project_page")
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.project_page)
@@ -307,8 +300,9 @@ class Ui_DesktopWindow(object):
         self.project_commands.setSelectionRectVisible(False)
         self.project_commands.setObjectName("project_commands")
         self.verticalLayout_2.addWidget(self.project_commands)
-        self.stack.addWidget(self.project_page)
-        self.border_layout.addWidget(self.stack)
+        self.apps_tab.addWidget(self.project_page)
+        self.tab_view.addWidget(self.apps_tab)
+        self.border_layout.addWidget(self.tab_view)
         self.footer = QtGui.QFrame(self.center)
         self.footer.setMouseTracking(True)
         self.footer.setFrameShape(QtGui.QFrame.NoFrame)
@@ -380,7 +374,7 @@ class Ui_DesktopWindow(object):
         self.actionRegenerate_Certificates.setObjectName("actionRegenerate_Certificates")
 
         self.retranslateUi(DesktopWindow)
-        self.stack.setCurrentIndex(0)
+        self.apps_tab.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(DesktopWindow)
         DesktopWindow.setTabOrder(self.projects, self.user_button)
         DesktopWindow.setTabOrder(self.user_button, self.search_button)
@@ -389,7 +383,6 @@ class Ui_DesktopWindow(object):
 
     def retranslateUi(self, DesktopWindow):
         DesktopWindow.setWindowTitle(QtGui.QApplication.translate("DesktopWindow", "Shotgun", None, QtGui.QApplication.UnicodeUTF8))
-        self.apps_button.setText(QtGui.QApplication.translate("DesktopWindow", "Apps", None, QtGui.QApplication.UnicodeUTF8))
         self.subheader_label.setText(QtGui.QApplication.translate("DesktopWindow", "PROJECTS", None, QtGui.QApplication.UnicodeUTF8))
         self.search_frame.setToolTip(QtGui.QApplication.translate("DesktopWindow", "Search Projects", None, QtGui.QApplication.UnicodeUTF8))
         self.search_magnifier.setToolTip(QtGui.QApplication.translate("DesktopWindow", "Search Projects", None, QtGui.QApplication.UnicodeUTF8))

python/tk_desktop/ui/preferences.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'preferences.ui'
 #
-#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
 

resources/desktop_dark.css
@@ -7,13 +7,24 @@ QWidget
     font-style: "Regular";
 }
 
-#header QPushButton, #header QPushButton:pressed
+#header QPushButton
 {
     background-color: transparent;
     font-size: 15px;
     font-style: "Condensed Light";
     border: none;
     padding: 15px 0px 10px 0px;
+}
+#header QPushButton:hover
+{
+    border-bottom: 2px solid rgb(85, 85, 85);
+}
+#header QPushButton:pressed
+{
+    border-bottom: 2px solid rgb(255, 255, 255);
+}
+#header QPushButton[active=true]
+{
     border-bottom: 2px solid rgb(195, 195, 195);
 }
 
@@ -102,7 +113,7 @@ QWidget
     background-color: rgb(40, 40, 40);
 }
 
-#stack
+#tab_view
 {
     background-color: rgb(40, 40, 40);
 }

commit a3a6d6d629a4445e5ea175fd6ec7882c0aba418e

[minor] Bug fix for panel apps running in project mode. 5fb929fd788ea0dc9082088c748aa139f3aa0ec6 introduced a regression where panel apps apps running in project mode would not open. An example of such an app is the python console in our basic config. This bug fix defers panel handling to the base class when in project mode, meaning that panels are currently only supported for the desktop site mode. This should restore the previous behaviour of panel apps running in project mode and the python console will pop up in a dialog window.

engine.py
@@ -83,8 +83,20 @@ class DesktopEngine(Engine):
         widget_class constructor.
         """
         if hasattr(self.__impl, "show_panel"):
+            # forward to site/projet implementation
             self.__impl.show_panel(panel_id, title, bundle, widget_class,
                                    *args, **kwargs)
+        else:
+            # fall back on base class implementation
+            super(DesktopEngine, self).show_panel(
+                panel_id,
+                title,
+                bundle,
+                widget_class,
+                *args,
+                **kwargs
+            )
+
 
     def destroy_engine(self):
         """ Clean up the engine """