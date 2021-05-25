259cd33f3cc472067b45acbf6eb6b88551e6bffc
Jean-Francois Boismenu <jean.francois.boismenu@autodesk.com>
2021-05-25 15:27:41 -0400

    reverted one shotgun rename

--- python/tk_desktop/notifications/first_launch_notification.py
+++ python/tk_desktop/notifications/first_launch_notification.py
@@ -22,7 +22,7 @@ class FirstLaunchNotification(Notification):
     _FIRST_LAUNCH_BANNER_VIEWED_ID = "first_desktop_launch_banner_viewed"
     SHOTGUN_DESKTOP_SUPPORT_PAGE_URL = (
         r"https://support.shotgunsoftware.com/hc/en-us/articles/115000068574"
-        r"#Getting%20started%20with%20ShotGrid%20Desktop"
+        r"#Getting%20started%20with%20Shotgun%20Desktop"
     )
 
     @classmethod
