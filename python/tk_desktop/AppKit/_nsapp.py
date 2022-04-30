01617985c5c005a6e02e3c4e7a997c1c4a1545fc
Norberto Moreno <norbermv.dev@gmail.com>
2022-04-29 23:08:30 -0500

    Add AppKit package and some of its dependencies to be able to import it for testing.

---
+++ python/tk_desktop/AppKit/_nsapp.py
@@ -0,0 +1,36 @@
+import objc
+from AppKit import __bundle__
+
+
+class _NSApp:
+    """
+    Helper class to emulate NSApp in Python.
+    """
+
+
+    def __getrealapp(self):
+        # import sys
+        # sys.path.append(
+        #     r"/Applications/PyCharm.app/Contents/debug-eggs/pydevd-pycharm.egg")
+        # import pydevd
+        # pydevd.settrace('localhost', port=5490, stdoutToServer=True,
+        #                 stderrToServer=True)
+        d = {}
+        #objc.loadBundleVariables(__bundle__, d, [("NSApp", b"@")])
+        return d.get("NSApp")
+
+    __class__ = property(lambda self: self.__getrealapp().__class__)
+
+    def __getattr__(self, name):
+        return getattr(self.__getrealapp(), name)
+
+    def __setattr__(self, name, value):
+        return setattr(self.__getrealapp(), name, value)
+
+    def __call__(self):
+        # Compatibility with previous versions.
+        return self.__getrealapp()
+
+
+NSApp = _NSApp()
+del _NSApp
