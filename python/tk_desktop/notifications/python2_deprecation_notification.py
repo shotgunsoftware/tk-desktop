7160f0846887d1d610c75e8244bd3b34b9a6b03e
Ariel Calzada <ariel.calzada.solano@autodesk.com>
2022-09-27 10:51:25 -0500

    Comment out new notification

---
+++ python/tk_desktop/notifications/python2_deprecation_notification.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2017 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+from .notification import Notification
+import sgtk
+
+logger = sgtk.platform.get_logger(__name__)
+
+
+class Python2DeprecationNotification(Notification):
+    """
+    Notification for python 2 deprecation.
+    """
+
+    _PYTHON2_DEPRECATION_ID = "python2-deprecation-notification"
+
+    def __init__(self, engine):
+        """
+        :param engine: Toolkit engine.
+        """
+        self._engine = engine
+
+    @classmethod
+    def create(cls, banner_settings, engine):
+        """
+        Notification factory for python 2 deprecation.
+
+        :param banner_settings: Dictionary of banner settings.
+        :param engine: Toolkit engine.
+
+        :returns: A :class:`Python2DeprecationNotification` instance, or ``None``.
+        """
+
+        if banner_settings.get(cls._PYTHON2_DEPRECATION_ID, {}).get(
+            "is_dismissed", False
+        ):
+            logger.debug("This banner has already been dismissed.")
+            return None
+        else:
+            logger.debug("Python 2 deprecation banner available")
+            return Python2DeprecationNotification(engine)
+
+    @property
+    def message(self):
+        """
+        Message to display.
+        """
+        return """
+        ShotGrid is ending support for Python 2 in ShotGrid Desktop on Nov 1 2022. 
+        Please upgrade to Python 3.9 before this date to ensure continuous support. See more information about this 
+        update <a href="https://community.shotgridsoftware.com/t/important-notice-upcoming-removal-of-python-2-7-and-3-7-interpreter-in-shotgrid-desktop/15168">here</a>.
+        """
+
+    @property
+    def unique_id(self):
+        """
+        Returns the unique identifier of a notification.
+        """
+        return self._PYTHON2_DEPRECATION_ID
+
+    def _dismiss(self, banner_settings):
+        """
+        Updates the ``banner_settings`` so this notification does not come back in the future.
+
+        :param banner_settings: Dictionary of the banners settings.
+        """
+        banner_settings.setdefault(self._PYTHON2_DEPRECATION_ID, {})["is_dismissed"] = True
