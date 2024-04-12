eab4d8c0262a2108e4654829e2fa9fa6d1aa1319
Julien Langlois <16244608+julien-lang@users.noreply.github.com>
2024-04-12 13:44:52 -0700

    SG-26914 Add banner centos 7 deprecation notice (#166)
    * Add banner for CentOS 7 end of life
    
    * Update message
    
    * Restrict banner to be displayed on EL7 OS only
    
    * Fixups
    
    * Fixup banner shows up on Rocky 8
    
    * fixup! Fixup banner shows up on Rocky 8
    
    * Black formater
    
    * Fixup Windows tests
    
    * Switch os-release parsing to platfor.freedesktop_os_release
    
    * Typo

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -10,7 +10,7 @@
 
 from __future__ import with_statement
 
-from tank_test.tank_test_base import TankTestBase, SealedMock
+from tank_test.tank_test_base import TankTestBase, SealedMock, mock
 from tank_test.tank_test_base import setUpModule  # noqa
 
 import notifications
@@ -231,6 +231,33 @@ class TestNotifications(TankTestBase):
         is_included = False
         for notif in notifs:
             if isinstance(notif, notifications.Python2DeprecationNotification):
+                is_included = True
+
+            self._notification_manager.dismiss(notif)
+
+        self.assertTrue(is_included)
+
+    def test_centos7_deprecation_notifs(self):
+        """
+        Test CentOS7 deprecation notification.
+        """
+
+        with mock.patch.object(
+            self._notification_manager,
+            "_get_banner_settings",
+            return_value={
+                notifications.FirstLaunchNotification._FIRST_LAUNCH_BANNER_VIEWED_ID: True,
+            },
+        ), mock.patch.object(
+            notifications.CentOS7DeprecationNotification,
+            "display_on_this_os",
+            return_value=True,
+        ):
+            notifs = self._notification_manager.get_notifications()
+
+        is_included = False
+        for notif in notifs:
+            if isinstance(notif, notifications.CentOS7DeprecationNotification):
                 is_included = True
 
             self._notification_manager.dismiss(notif)
