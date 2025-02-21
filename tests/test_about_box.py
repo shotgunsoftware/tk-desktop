ed7113be2ef3f11908b8b89ff799a8e14a0371fc
Julien Langlois <julien.langlois@autodesk.com>
2025-02-21 11:40:11 -0800

    Fix tests

--- tests/test_about_box.py
+++ tests/test_about_box.py
@@ -79,11 +79,10 @@ def test_3rd_party_links(licence_file_links):
     "expected_url",
     [
         "https://github.com/shotgunsoftware/python-api/tree/master/software_credits",
-        "https://github.com/shotgunsoftware/tk-3dsmax/tree/master/software_credits",
-        "https://github.com/shotgunsoftware/tk-3dsmaxplus/tree/master/software_credits",
         "https://github.com/shotgunsoftware/tk-alias/tree/master/software_credits",
         "https://github.com/shotgunsoftware/tk-core/tree/master/software_credits",
         "https://github.com/shotgunsoftware/tk-framework-adobe/tree/master/software_credits",
+        "https://github.com/shotgunsoftware/tk-framework-alias/tree/master/software_credits",
         "https://github.com/shotgunsoftware/tk-framework-desktopclient/tree/master/software_credits",
         "https://github.com/shotgunsoftware/tk-framework-desktopserver/tree/master/software_credits",
         "https://github.com/shotgunsoftware/tk-houdini/tree/master/software_credits",
