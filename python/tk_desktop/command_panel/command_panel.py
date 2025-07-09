shotgunsoftware/tk-desktop

commit e8e31813baa9aaf098820018ea369c0a9688f112

SG-37924 Replace utcnow() for Python 3.12 (#197)
* Replace `utcnow()` for Python 3.12

* format

python/tk_desktop/command_panel/command_panel.py
@@ -8,7 +8,7 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-from datetime import datetime
+from datetime import datetime, timezone
 from sgtk.platform.qt import QtGui, QtCore
 
 from .shared import MAX_RECENTS
@@ -185,7 +185,7 @@ class CommandPanel(QtGui.QWidget):
             return
 
         command_name = str(command_name)
-        self._recents[command_name] = {"timestamp": datetime.utcnow()}
+        self._recents[command_name] = {"timestamp": datetime.now(timezone.utc)}
         self._store_recents()
         self._refresh_recent_list(command_name)
         self._restrict_recent_buttons(self._get_optimal_width())

tests/test_command_panel.py
@@ -84,7 +84,13 @@ def test_sections_sorted(show_recents, commands):
     view = CommandPanel(
         sgtk.platform.qt.QtGui.QScrollArea(),
         Settings(
-            {PROJECT_KEY: {"command 0": {"timestamp": datetime.datetime.utcnow()}}}
+            {
+                PROJECT_KEY: {
+                    "command 0": {
+                        "timestamp": datetime.datetime.now(datetime.timezone.utc)
+                    }
+                }
+            }
         ),
     )
     view.configure(PROJECT, groups, show_recents=show_recents)
@@ -126,7 +132,13 @@ def test_clear_deletes_all_but_stretcher():
     view = CommandPanel(
         sgtk.platform.qt.QtGui.QScrollArea(),
         Settings(
-            {PROJECT_KEY: {"maya_2020": {"timestamp": datetime.datetime.utcnow()}}}
+            {
+                PROJECT_KEY: {
+                    "maya_2020": {
+                        "timestamp": datetime.datetime.now(datetime.timezone.utc)
+                    }
+                }
+            }
         ),
     )
     view.configure(PROJECT, ["Creative Tools", "Editorial"], show_recents=True)

python/tk_desktop/project_model.py
@@ -321,7 +321,9 @@ class SgProjectModel(ShotgunModel):
         # Update the data in the model
         item = self.item_from_entity("Project", project["id"])
         # set to unix seconds rather than datetime to be compatible with Shotgun model
-        utc_now_epoch = time.mktime(datetime.datetime.utcnow().utctimetuple())
+        utc_now_epoch = time.mktime(
+            datetime.datetime.now(datetime.timezone.utc).utctimetuple()
+        )
         project["last_accessed_by_current_user"] = utc_now_epoch
         item.setData(project, ShotgunModel.SG_DATA_ROLE)
 

commit e7ef247939c51aadb4415634af48d780688ecf8f

Make sure our datetime objects are created in naive mode
Critical since the info is then cached on disk in a string format
that does not include the timezone so, once they are loaded back
from disk, we have naive datetime  object.

Fixup regression introduced in #197:

  TypeError: can't compare offset-naive and offset-aware datetimes

python/tk_desktop/command_panel/command_panel.py
@@ -185,7 +185,7 @@ class CommandPanel(QtGui.QWidget):
             return
 
         command_name = str(command_name)
-        self._recents[command_name] = {"timestamp": datetime.now(timezone.utc)}
+        self._recents[command_name] = {"timestamp": datetime.now()}
         self._store_recents()
         self._refresh_recent_list(command_name)
         self._restrict_recent_buttons(self._get_optimal_width())

tests/test_command_panel.py
@@ -87,7 +87,7 @@ def test_sections_sorted(show_recents, commands):
             {
                 PROJECT_KEY: {
                     "command 0": {
-                        "timestamp": datetime.datetime.now(datetime.timezone.utc)
+                        "timestamp": datetime.datetime.now()
                     }
                 }
             }
@@ -135,7 +135,7 @@ def test_clear_deletes_all_but_stretcher():
             {
                 PROJECT_KEY: {
                     "maya_2020": {
-                        "timestamp": datetime.datetime.now(datetime.timezone.utc)
+                        "timestamp": datetime.datetime.now()
                     }
                 }
             }