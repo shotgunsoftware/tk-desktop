5e0e49720944be6523cd57396ee950dee08b7067
chenm1 <ming.chen@autodesk.com>
2026-05-28 21:33:08 -0700

    Address Julien's comment

--- hooks/tk-multi-publish2/flowam/publish_to_flow.py
+++ hooks/tk-multi-publish2/flowam/publish_to_flow.py
@@ -8,8 +8,8 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import pprint
 import os
+import pprint
 
 import sgtk
 
